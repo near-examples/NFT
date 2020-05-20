d2c04f6e1fab61f17fd3f07293c47072d580c6ad
Sherif Abushadi <sherif@amgando.org>
2020-05-20 10:31:23 +0300

    tests passing

--- contracts/assemblyscript/nep4/__tests__/main.unit.spec.ts
+++ contracts/assemblyscript/nep4/__tests__/main.unit.spec.ts
@@ -8,45 +8,135 @@ import {
   get_token_owner,
 } from '../main'
 
+import { VM, Context } from 'near-sdk-as'
+
 const alice = 'alice'
 const bob = 'bob'
 
 describe('grant_access', () => {
-  xit('grants access to the given account_id for all the tokens that account has', () => {
+  it('grants access to the given account_id for all the tokens that account has', () => {
+    const aliceToken = nonSpec.mint_to(alice)
+    Context.setPredecessor_account_id(alice)
+
+    grant_access(bob)
+
+    expect(check_access(bob)).toBeTruthy()
   })
 
-  xit('requires the caller of the function to have access to the token.', () => {
+  it('requires the caller of the function to have access to the token.', () => {
+    expect(() => {
+      const aliceToken = nonSpec.mint_to(alice)
+      Context.setPredecessor_account_id(bob)
+
+      grant_access(bob);
+    }).toThrow(nonSpec.ERROR_NO_TOKENS_CONTROLLED)
   })
 });
 
 describe('revoke_access', () => {
-  xit('revokes access to the given `accountId` for the given `tokenId`', () => {
+  it('revokes access to the given `accountId` for the given `tokenId`', () => {
+    const aliceToken = nonSpec.mint_to(alice)
+    Context.setPredecessor_account_id(alice)
+
+    grant_access(bob);
+    expect(check_access(bob)).toBeTruthy()
+
+    revoke_access(bob);
+    expect(check_access(bob)).toBeFalsy()
   })
 
-  xit('requires caller of the function to have access to the token.', () => {
+  it('requires caller of the function to have access to the token.', () => {
+    expect(() => {
+      const aliceToken = nonSpec.mint_to(alice)
+      Context.setPredecessor_account_id(bob)
+
+      revoke_access(bob);
+    }).toThrow(nonSpec.ERROR_NO_TOKENS_CONTROLLED)
   })
 })
 
 describe('transfer_from', () => {
-  xit('transfers the given `tokenId` from the given `owner_id` to  `new_owner_id`', () => {
+  it('transfers the given `token_id` from the given `owner_id` to  `new_owner_id`', () => {
+    const aliceToken = nonSpec.mint_to(alice)
+    Context.setPredecessor_account_id(alice)
+
+    expect(get_token_owner(aliceToken)).toBe(alice)
+    expect(get_token_owner(aliceToken)).not.toBe(bob)
+
+    transfer_from(alice, bob, aliceToken)
+
+    expect(get_token_owner(aliceToken)).toBe(bob)
+    expect(get_token_owner(aliceToken)).not.toBe(alice)
   })
 
-  xit('requires the caller of the function to have access to the token.', () => {
+  it('requires the caller of the function to have access to the token.', () => {
+    expect(() => {
+      const aliceToken = nonSpec.mint_to(alice)
+      Context.setPredecessor_account_id(bob)
+
+      transfer_from(alice, bob, aliceToken);
+    }).toThrow(nonSpec.ERROR_CALLER_ID_DOES_NOT_MATCH_EXPECTATION)
+  })
+
+  it('expects the token to exist', () => {
+    expect(() => {
+      const fakeToken = -1
+      Context.setPredecessor_account_id(bob)
+
+      transfer_from(alice, bob, fakeToken);
+    }).toThrow(nonSpec.ERROR_INVALID_TOKEN_ID)
   })
 })
 
 describe('transfer', () => {
-  xit('transfers the given `token_id` to the given `owner_id`', () => {
+  it('transfers the given `token_id` to the given `owner_id`', () => {
+
+    const aliceToken = nonSpec.mint_to(alice)
+    Context.setPredecessor_account_id(alice)
+
+    expect(get_token_owner(aliceToken)).toBe(alice)
+    expect(get_token_owner(aliceToken)).not.toBe(bob)
+
+    transfer(bob, aliceToken)
+
+    expect(get_token_owner(aliceToken)).toBe(bob)
+    expect(get_token_owner(aliceToken)).not.toBe(alice)
   })
 
-  xit('requires the caller of the function to have access to the token', () => {
+  it('requires the caller of the function to have access to the token', () => {
+    expect(() => {
+      const aliceToken = nonSpec.mint_to(alice)
+      Context.setPredecessor_account_id(bob)
+
+      transfer(bob, aliceToken);
+    }).toThrow(nonSpec.ERROR_CLAIMED_OWNER_DOES_NOT_OWN_TOKEN)
+  })
+
+  it('expects the token to exist', () => {
+    expect(() => {
+      const fakeToken = -1
+      Context.setPredecessor_account_id(bob)
+
+      transfer(bob, fakeToken);
+    }).toThrow(nonSpec.ERROR_INVALID_TOKEN_ID)
   })
 })
 
 describe('check_access', () => {
-  xit('returns true if caller of the function has access to the token', () => {
+  it('returns true if caller of the function has access to the token', () => {
+    const aliceToken = nonSpec.mint_to(alice)
+    Context.setPredecessor_account_id(alice)
+
+    grant_access(bob)
+
+    expect(check_access(bob)).toBeTruthy()
   })
-  xit('returns false if caller of function does not have access', () => {
+
+  it('returns false if caller of function does not have access', () => {
+    const aliceToken = nonSpec.mint_to(alice)
+    Context.setPredecessor_account_id(alice)
+
+    expect(check_access(bob)).toBeFalsy()
   })
 })
 
@@ -60,5 +150,37 @@ describe('get_token_owner', () => {
 
     expect(get_token_owner(bobToken)).toBe(bob)
     expect(get_token_owner(bobToken)).not.toBe(alice)
+  })
+})
+
+// not sure if these are needed?
+describe('nonSpec interface', () => {
+  it('should include several constants', () => {
+    expect(nonSpec.MAX_SUPPLY).toBeGreaterThan(0)
+    expect(nonSpec.ERROR_NO_TOKENS_CONTROLLED).toBeTruthy()
+    expect(nonSpec.ERROR_CALLER_ID_DOES_NOT_MATCH_EXPECTATION).toBeTruthy()
+    expect(nonSpec.ERROR_INVALID_TOKEN_ID).toBeTruthy()
+    expect(nonSpec.ERROR_CLAIMED_OWNER_DOES_NOT_OWN_TOKEN).toBeTruthy()
+    expect(nonSpec.ERROR_MAXIMUM_TOKEN_LIMIT_REACHED).toBeTruthy()
+  })
+
+  it('should expose an interface for minting', () => {
+    expect(() => {
+      nonSpec.mint_to(alice)
+    }).not.toThrow()
+  })
+
+  it('should throw if we attempt to mint more than the MAX_SUPPLY', () => {
+    expect(() => {
+      let limit = nonSpec.MAX_SUPPLY
+      while(limit-- > 0) {
+        nonSpec.mint_to(alice)
+      }
+    }).not.toThrow()
+
+    // minting one more than the max should throw
+    expect(() => {
+      nonSpec.mint_to(alice)
+    }).toThrow(nonSpec.ERROR_MAXIMUM_TOKEN_LIMIT_REACHED)
   })
 })
--- contracts/assemblyscript/nep4/main.ts
+++ contracts/assemblyscript/nep4/main.ts
@@ -1,12 +1,27 @@
-import { PersistentMap, storage } from 'near-sdk-as'
+import { logging, PersistentMap, storage, context } from 'near-sdk-as'
+
+/**************************/
+/* DATA TYPES AND STORAGE */
+/**************************/
 
 type AccountId = string
 type TokenId = u64
 
-const MAX_SUPPLY = 1_000
+export const MAX_SUPPLY = u64(1_000)
 
-const tokenToOwner = new PersistentMap<TokenId, AccountId>('a')
+export const tokenToOwner = new PersistentMap<TokenId, AccountId>('a')
+export const escrowAccess = new PersistentMap<AccountId, AccountId[]>('b')
 const TOTAL_SUPPLY = 'c'
+
+/******************/
+/* ERROR MESSAGES */
+/******************/
+
+export const ERROR_NO_TOKENS_CONTROLLED = "Caller does not control any tokens."
+export const ERROR_CALLER_ID_DOES_NOT_MATCH_EXPECTATION = "Caller ID does not match expectation."
+export const ERROR_INVALID_TOKEN_ID = "Invalid token identifier"
+export const ERROR_CLAIMED_OWNER_DOES_NOT_OWN_TOKEN = "Claimed owner does not own token"
+export const ERROR_MAXIMUM_TOKEN_LIMIT_REACHED = "Maximum token limit reached."
 
 /******************/
 /* CHANGE METHODS */
@@ -16,34 +31,90 @@ const TOTAL_SUPPLY = 'c'
 // Requirements:
 // * The caller of the function (`predecessor_id`) should have access to the tokens.
 export function grant_access(escrow_account_id: string): void {
+  const predecessor = context.predecessor
+
+  // fetch all accounts with escrow access to predecessor tokens
+  assert(escrowAccess.contains(predecessor), ERROR_NO_TOKENS_CONTROLLED)
+  const escrowAccounts = escrowAccess.getSome(predecessor)
+
+  // add escrow_account_id to list of escrow accounts
+  escrowAccounts.push(escrow_account_id)
+
+  // grant escrow account access to all predecessor tokens
+  escrowAccess.set(predecessor, escrowAccounts)
 }
 
 // Revoke the access to the given `accountId` for the given `tokenId`.
 // Requirements:
 // * The caller of the function (`predecessor_id`) should have access to the token.
 export function revoke_access(escrow_account_id: string): void {
+  const predecessor = context.predecessor
+
+  // fetch escrow_account_id from list of escrow accounts
+  assert(escrowAccess.contains(predecessor), ERROR_NO_TOKENS_CONTROLLED)
+  const escrowAccounts = escrowAccess.getSome(predecessor)
+
+  // remove the escrow account from list of accounts with access to owner's tokens
+  // note we can't use Array<string>.filter() since it's not implemented in AssemblyScript
+  // and only number types seem to work with Array<T>.filter()
+  // also there are no clojures in AssemblyScript yet so passing in the value to filter on
+  // seems like a non-starter.  for loops FTW!
+  let filteredAccounts: AccountId[] = []
+  for (let index = 0; index < escrowAccounts.length; index++) {
+    const account = escrowAccounts[index]
+    if(account != escrow_account_id) {
+      filteredAccounts.push(account)
+    }
+  }
+
+  // commit the removal to the escrow registry
+  escrowAccess.set(predecessor, filteredAccounts)
 }
 
-// Transfer the given `tokenId` from the given `accountId`.  Account `newAccountId` becomes the new owner.
+// Transfer the given `token_id` from the given `owner_id`.  Account `new_owner_id` becomes the new owner.
 // Requirements:
 // * The caller of the function (`predecessor_id`) should have access to the token.
-export function transfer_from(owner_id: string, new_owner_id: string, tokenId: TokenId): void {
+export function transfer_from(owner_id: string, new_owner_id: string, token_id: TokenId): void {
+  const predecessor = context.predecessor
+  assert(owner_id == predecessor, ERROR_CALLER_ID_DOES_NOT_MATCH_EXPECTATION)
+
+  // delegate to transfer function since this seems redundant
+  transfer(new_owner_id, token_id)
 }
 
-// Transfer the given `tokenId` to the given `accountId`.  Account `accountId` becomes the new owner.
+// Transfer the given `token_id` to the given `new_owner_id`.  Account `new_owner_id` becomes the new owner.
 // Requirements:
 // * The caller of the function (`predecessor_id`) should have access to the token.
 export function transfer(new_owner_id: string, token_id: TokenId): void {
-}
+  const predecessor = context.predecessor
 
+  // fetch token owner and assert ownership
+  assert(tokenToOwner.contains(token_id), ERROR_INVALID_TOKEN_ID)
+  const owner = tokenToOwner.getSome(token_id)
+  assert(owner == predecessor, ERROR_CLAIMED_OWNER_DOES_NOT_OWN_TOKEN)
+
+  // assign new owner to token
+  tokenToOwner.set(token_id, new_owner_id)
+}
 
 /****************/
 /* VIEW METHODS */
 /****************/
 
 // Returns `true` or `false` based on caller of the function (`predecessor_id`) having access to a user's tokens
-export function check_access(account_id: string): boolean {
-  return false
+export function check_access(account_id: string): bool {
+  const owner = context.predecessor
+
+  // confirm the caller has been minted at least one token
+  if(!escrowAccess.contains(owner)) {
+    // caller has never been minted a token and does not appear in the registry
+    return false
+  } else {
+    // fetch list of escrow accounts with access to owner's tokens
+    const escrowAccounts = escrowAccess.getSome(owner)
+    // return result of check
+    return escrowAccounts.includes(account_id)
+  }
 }
 
 // Get an individual owner by given `tokenId`.
@@ -51,14 +122,25 @@ export function get_token_owner(token_id: TokenId): string {
   return tokenToOwner.getSome(token_id)
 }
 
-
 /********************/
 /* NON-SPEC METHODS */
 /********************/
 
 export function mint_to(owner_id: AccountId): u64 {
+  // fetch the next tokenId
   const tokenId = storage.getPrimitive<u64>(TOTAL_SUPPLY, 0)
+  const nextId = tokenId + 1
+
+  // enforce token limits
+  assert(nextId <= MAX_SUPPLY, ERROR_MAXIMUM_TOKEN_LIMIT_REACHED)
+
+  // assign ownership
   tokenToOwner.set(tokenId, owner_id)
-  storage.set<u64>(TOTAL_SUPPLY, tokenId + 1)
+  escrowAccess.set(owner_id, [])
+
+  // increment and store the next tokenId
+  storage.set<u64>(TOTAL_SUPPLY, nextId)
+
+  // return the tokenId
   return tokenId
 }
