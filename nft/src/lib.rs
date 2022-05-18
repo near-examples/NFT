d8e5f17fe5f9f0bb7ff88dc05fdfed9bd2124503
Dennis <10233439+idea404@users.noreply.github.com>
2022-05-18 17:27:37 +0100

    add missing import for unit testing

--- nft/src/lib.rs
+++ nft/src/lib.rs
@@ -117,6 +117,7 @@ impl NonFungibleTokenMetadataProvider for Contract {
 mod tests {
     use near_sdk::test_utils::{accounts, VMContextBuilder};
     use near_sdk::testing_env;
+    use near_sdk::MockedBlockchain;
 
     use super::*;
 
