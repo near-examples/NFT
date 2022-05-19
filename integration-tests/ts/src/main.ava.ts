952f9f7b24016cbdd925cb25e3d79c9ab499eba0
Dennis <10233439+idea404@users.noreply.github.com>
2022-05-19 10:31:32 +0100

    ts test: Enum nft tokens for owner

--- integration-tests/ts/src/main.ava.ts
+++ integration-tests/ts/src/main.ava.ts
@@ -474,3 +474,34 @@ test('Enum nft supply for owner', async test => {
     ownerNumTokens = new BN(await nft.view('nft_supply_for_owner', { account_id: root }));
     test.deepEqual(ownerNumTokens, new BN(4));
 });
+
+test('Enum nft tokens for owner', async test => {
+    const { root, alice, nft } = test.context.accounts;
+    await mint_more(root, nft);
+
+    // Get tokens from account with no NFTs
+    let ownerTokens: any[] = await nft.view('nft_tokens_for_owner', { account_id: alice });
+    test.deepEqual(ownerTokens.length, 0);
+
+    // Get tokens with no optional args
+    ownerTokens = await nft.view('nft_tokens_for_owner', { account_id: root });
+    test.deepEqual(ownerTokens.length, 4);
+
+    // With from_index and no limit
+    ownerTokens = await nft.view('nft_tokens_for_owner', { account_id: root, from_index: new BN(2) });
+    test.deepEqual(ownerTokens.length, 2);
+    test.is(ownerTokens[0].token_id, '2');
+    test.is(ownerTokens[1].token_id, '3');
+
+    // With from_index and limit 1
+    ownerTokens = await nft.view('nft_tokens_for_owner', { account_id: root, from_index: new BN(1), limit: 1 });
+    test.deepEqual(ownerTokens.length, 1);
+    test.is(ownerTokens[0].token_id, '1');
+
+    // No from_index but limit 3
+    ownerTokens = await nft.view('nft_tokens_for_owner', { account_id: root, limit: 3 });
+    test.deepEqual(ownerTokens.length, 3);
+    test.is(ownerTokens[0].token_id, '0');
+    test.is(ownerTokens[1].token_id, '1');
+    test.is(ownerTokens[2].token_id, '2');
+});
