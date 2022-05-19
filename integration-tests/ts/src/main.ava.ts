37e6a8d8a1dd8fa9d93672679806fdd913342569
Dennis <10233439+idea404@users.noreply.github.com>
2022-05-19 10:13:37 +0100

    ts test: Transfer call fast return to sender

--- integration-tests/ts/src/main.ava.ts
+++ integration-tests/ts/src/main.ava.ts
@@ -334,3 +334,21 @@ test('Simple transfer', async test => {
     token = await nft.view('nft_token', { token_id: '0' });
     test.is(token.owner_id, alice.accountId);
 });
+
+test('Transfer call fast return to sender', async test => {
+    const { root, tokenReceiver, nft } = test.context.accounts;
+    await root.call(
+        nft,
+        'nft_transfer_call',
+        {
+            receiver_id: tokenReceiver,
+            token_id: '0',
+            memo: 'transfer & call',
+            msg: 'return-it-now',
+        },
+        { attachedDeposit: '1', gas: tGas(150) },
+    );
+
+    const token: any = await nft.view('nft_token', { token_id: '0' });
+    test.is(token.owner_id, root.accountId);
+});
