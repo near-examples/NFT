962bc16012a3b430a3f7eb68ec2bec856f36f956
Dennis <10233439+idea404@users.noreply.github.com>
2022-05-23 13:26:12 +0100

    remove script files for testing move logic to yml

--- scripts/integration_tests.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-cd ../integration-tests
-echo running Rust integration-tests
-cd rs && cargo run --example integration-tests
-echo running TypeScript integration-tests
-cd ../ts && yarn && yarn test 
--- scripts/unit_tests.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-cd ../nft
-cargo test -- --nocapture --color=always
