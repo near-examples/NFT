61294c5fcfc285cdf17d1f2292ef5a25375f7151
shivaPeri <shivaperi47@gmail.com>
2022-06-26 15:20:40 -0400

    tried direct emshes instead of instancing

--- frontend/src/routes/index.svelte
+++ frontend/src/routes/index.svelte
@@ -18,38 +18,36 @@
 	// Functional TODO:
 	// orthographic views (top, bottom, left, right)
 
-	import {
-		SphereGeometry,
-		CylinderGeometry,
-		DoubleSide,
-		Vector3,
-		MeshStandardMaterial
-	} from 'three';
+	import { SphereGeometry, CylinderGeometry, Vector3, MeshStandardMaterial } from 'three';
 	import {
 		Canvas,
 		DirectionalLight,
-		Group,
 		HemisphereLight,
 		Instance,
 		InstancedMesh,
 		Mesh,
 		OrbitControls,
-		OrthographicCamera
+		OrthographicCamera,
+		ContextBridge,
+		type Position,
+		type ThrelteContext
 	} from 'threlte';
 
 	const sphereGeometry = new SphereGeometry(1);
 	const cylinderGeometry = new CylinderGeometry(1, 1, 1);
+
+	let ctx: ThrelteContext | undefined;
 
 	let path: TurtlePath = defaultPath;
 	let preview: TurtlePath = previewPath;
 	preview.points = generatePreviewPoints($parameters);
 	computeCylinders(preview);
 
-	let cameraPos = { x: 1, y: 1, z: 1 };
+	let cameraPos: Position = { x: 1, y: 1, z: 1 };
 	let bounds: Bounds = defaultBounds;
-
 	let pos: PosType = [0, 0, 0];
-	let steps = 0;
+	let steps: number = 0;
+
 	const start = async () => {
 		while ($parameters.running && steps < MAX_STEPS) {
 			const [deltaX, deltaY, deltaZ] = calcChangeInPosVec($parameters);
@@ -72,9 +70,13 @@
 		}, 400);
 	});
 
+	let thing = {};
+
 	// called to reset entire state
 	// called on parameter update from Controls.svelte
 	const reset = () => {
+		console.log(ctx?.scene.children);
+
 		console.log('reset called');
 		path = defaultPath;
 		preview = previewPath;
@@ -87,15 +89,22 @@
 		pos = [0, 0, 0];
 		steps = 0;
 
-		setTimeout(() => {
-			start();
-		}, 400);
+		thing = {};
+
+		parameters.subscribe((newParams) => {
+			setTimeout(() => {
+				start();
+			}, 400);
+		});
 	};
 </script>
 
 <div class="container">
+	<input type="range" bind:value={path.width} min="0.01" max="10" step=".01" />
 	<Controls on:paramchange={(e) => reset()} />
 	<Canvas>
+		<ContextBridge bind:ctx />
+
 		<OrthographicCamera far={1000000000000} position={cameraPos}>
 			<OrbitControls enableDamping autoRotate autoRotateSpeed={0.5} target={getCentroid(bounds)} />
 		</OrthographicCamera>
@@ -103,26 +112,45 @@
 		<DirectionalLight shadow color={'#EDBD9C'} position={{ x: -15, y: 45, z: 20 }} />
 		<HemisphereLight skyColor={0x4c8eac} groundColor={0xac844c} intensity={0.6} />
 
-		{#each [path, preview] as turtle}
-			<Group>
-				<!-- spheres -->
-				<InstancedMesh castShadow receiveShadow geometry={sphereGeometry} material={turtle.mat}>
-					{#each turtle.points as p}
-						<Instance position={p} scale={{ x: turtle.width, y: turtle.width, z: turtle.width }} />
-					{/each}
-				</InstancedMesh>
+		{#each [path, preview] as turtle, i}
+			<!-- spheres -->
+			<!-- <InstancedMesh castShadow receiveShadow geometry={sphereGeometry} material={turtle.mat}>
+				{#each turtle.points as p}
+					<Instance position={p} scale={turtle.width} />
+				{/each}
+			</InstancedMesh> -->
+			{#each turtle.points as p}
+				<Mesh
+					castShadow
+					receiveShadow
+					geometry={sphereGeometry}
+					material={turtle.mat}
+					position={p}
+					scale={turtle.width}
+				/>
+			{/each}
 
-				<!-- cylinders -->
-				<InstancedMesh castShadow receiveShadow geometry={cylinderGeometry} material={turtle.mat}>
-					{#each turtle.cylinders as c}
-						<Instance
-							position={c.position}
-							rotation={c.rotation}
-							scale={{ x: turtle.width, y: c.scale.y, z: turtle.width }}
-						/>
-					{/each}
-				</InstancedMesh>
-			</Group>
+			<!-- cylinders -->
+			<!-- <InstancedMesh castShadow receiveShadow geometry={cylinderGeometry} material={turtle.mat}>
+				{#each turtle.cylinders as c}
+					<Instance
+						position={c.position}
+						rotation={c.rotation}
+						scale={{ x: turtle.width, y: c.scale.y, z: turtle.width }}
+					/>
+				{/each}
+			</InstancedMesh> -->
+			{#each turtle.cylinders as c}
+				<Mesh
+					castShadow
+					receiveShadow
+					geometry={cylinderGeometry}
+					material={turtle.mat}
+					position={c.position}
+					rotation={c.rotation}
+					scale={{ x: turtle.width, y: c.scale.y, z: turtle.width }}
+				/>
+			{/each}
 		{/each}
 
 		<!-- origin -->
