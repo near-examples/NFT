b3983c10c99ef5582003c72b4b4320bb649a245e
Dennis <10233439+idea404@users.noreply.github.com>
2022-08-10 14:53:47 +0000

    add echo messages

--- .github/scripts/readme-quick-deploy.sh
+++ .github/scripts/readme-quick-deploy.sh
@@ -10,9 +10,8 @@ fi
 
 echo ==== Set dev account env variable ====
 source neardev/dev-account.env
-
-echo ==== Check dev account env variable ====
 TEXT=$(echo $CONTRACT_NAME)
+echo $TEXT # TODO: remove
 if [[ ! "$TEXT" =~ .*"dev-".* ]]; then 
     echo -e "\033[0;31m FAIL \033[0m"
     exit 1 
@@ -22,7 +21,8 @@ fi
 
 echo ==== Initialize contract using the new method ====
 TEXT=$(near call $CONTRACT_NAME new_default_meta '{"owner_id": "'$CONTRACT_NAME'"}' --accountId $CONTRACT_NAME)
-if [[ ! "$TEXT" =~ .*"new_default_meta".* ]]; then # TODO: improve precision
+echo $TEXT # TODO: improve precision and remove
+if [[ ! "$TEXT" =~ .*"new_default_meta".* ]]; then 
     echo -e "\033[0;31m FAIL \033[0m"
     exit 1 
 else 
@@ -31,6 +31,7 @@ fi
 
 echo ==== View contract metadata ====
 TEXT=$(near view $CONTRACT_NAME nft_metadata)
+echo $TEXT # TODO: improve precision and remove
 if [[ ! "$TEXT" =~ .*"Olympus Mons".* ]]; then 
     echo -e "\033[0;31m FAIL \033[0m"
     exit 1 
@@ -40,7 +41,8 @@ fi
 
 echo ==== Mint NFT ==== 
 TEXT=$(near call $CONTRACT_NAME nft_mint '{"token_id": "0", "receiver_id": "'$ID'", "token_metadata": { "title": "Olympus Mons", "description": "Tallest mountain in charted solar system", "media": "https://upload.wikimedia.org/wikipedia/commons/thumb/0/00/Olympus_Mons_alt.jpg/1024px-Olympus_Mons_alt.jpg", "copies": 1}}' --accountId $CONTRACT_NAME --deposit 0.1)
-if [[ ! "$TEXT" =~ .*"ransaction".* ]]; then # TODO: improve precision
+echo $TEXT # TODO: improve precision and remove
+if [[ ! "$TEXT" =~ .*"ransaction".* ]]; then
     echo -e "\033[0;31m FAIL \033[0m"
     exit 1 
 else 
@@ -49,7 +51,8 @@ fi
 
 echo ==== Create Sub Account ====
 TEXT=$(near create-account alice.$CONTRACT_NAME --masterAccount $CONTRACT_NAME --initialBalance 10)
-if [[ ! "$TEXT" =~ .*"alice.$CONTRACT_NAME".* ]]; then # TODO: improve precision
+echo $TEXT # TODO: improve precision and remove
+if [[ ! "$TEXT" =~ .*"alice.$CONTRACT_NAME".* ]]; then
     echo -e "\033[0;31m FAIL \033[0m"
     exit 1 
 else 
@@ -58,7 +61,8 @@ fi
 
 echo ==== Check Sub Account for Tokens ====
 TEXT=$(near view $CONTRACT_NAME nft_tokens_for_owner '{"account_id": "'alice.$CONTRACT_NAME'"}')
-if [[ ! "$TEXT" =~ .*"Olympus Mons".* ]]; then # TODO: improve precision
+echo $TEXT # TODO: improve precision and remove
+if [[ ! "$TEXT" =~ .*"Olympus Mons".* ]]; then
     echo -e "\033[0;31m FAIL \033[0m"
     exit 1 
 else 
@@ -67,7 +71,8 @@ fi
 
 echo ==== Transfer NFT ====
 TEXT=$(near call $CONTRACT_NAME nft_transfer '{"token_id": "0", "receiver_id": "alice.'$CONTRACT_NAME'", "memo": "transfer ownership"}' --accountId $CONTRACT_NAME --depositYocto 1)
-if [[ ! "$TEXT" =~ .*"ransaction".* ]]; then # TODO: improve precision
+echo $TEXT # TODO: improve precision and remove
+if [[ ! "$TEXT" =~ .*"ransaction".* ]]; then
     echo -e "\033[0;31m FAIL \033[0m"
     exit 1 
 else 
